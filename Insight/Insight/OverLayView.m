45d83e466585cd99e55b592e014212d6c25a08b0
kbharathala <kbharathala@gmail.com>
2015-11-06 23:42:06 -0500

    added gear and touch actions

--- Insight/Insight/OverLayView.m
+++ Insight/Insight/OverLayView.m
@@ -10,11 +10,27 @@
 
 @implementation OverLayView
 
-
-// Only override drawRect: if you perform custom drawing.
-// An empty implementation adversely affects performance during animation.
 - (void)drawRect:(CGRect)rect {
     self.backgroundColor = [UIColor clearColor];
+    
+    UIButton *testButton = [UIButton buttonWithType:UIButtonTypeCustom];
+    [testButton setTitle:@"TEST" forState:UIControlStateNormal];
+    [testButton setTitleColor:[UIColor blueColor] forState:UIControlStateNormal];
+    [testButton sizeToFit];
+    [testButton setCenter:self.center];
+    testButton.transform = CGAffineTransformMakeRotation(M_PI_2);
+    [self addSubview:testButton];
+    
+    UIButton *settingsButton = [UIButton buttonWithType:UIButtonTypeCustom];
+    [settingsButton setFrame:CGRectMake(self.frame.size.width-50, self.frame.size.height-50, 40, 40)];
+    [settingsButton setImage:[UIImage imageNamed:@"gear.png"] forState:UIControlStateNormal];
+    settingsButton.transform = CGAffineTransformMakeRotation(M_PI_2);
+    [settingsButton addTarget:self action:@selector(openSettings) forControlEvents:UIControlEventTouchUpInside];
+    [self addSubview:settingsButton];
+}
+
+-(void) openSettings {
+    NSLog(@"Clicked Settings");
     
     UIButton *testButton = [UIButton buttonWithType:UIButtonTypeCustom];
     [testButton setTitle:@"TEST" forState:UIControlStateNormal];
